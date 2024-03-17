81a9a89229b3b05eec6533f5c3261cf77979b29f
HAndres8 <andresfhernandez999@gmail.com>
2024-03-17 10:41:00 -0500

    The disclaimer and the navigation menu are added

--- src/app/components/tags/tags.component.ts
+++ src/app/components/tags/tags.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./tags.component.css']
 })
 export class TagsComponent {
-  
+  showData: any[] = [];
+  myData: any[] = [];
+  data: any[] = [];
+  linkActive: number = 0;
+
+  ngOnInit(): void{
+    this.data = [
+      {'name:': 'Tokino Sora', 'gen': 'gen-0'},
+      {'name:': 'Robocosan', 'gen': 'gen-0'},
+      {'name:': 'Aki Rosenthal', 'gen': 'gen-1'},
+      {'name:': 'Akai Haato', 'gen': 'gen-1'},
+      {'name:': 'Shirakami Fubuki', 'gen': 'gen-1'},
+      {'name:': 'Natsuiro Matsuri', 'gen': 'gen-1'},
+      {'name:': 'Minato Aqua', 'gen': 'gen-2'}
+    ]
+  }
+
+  filterTalents(nbutton: number, gen: string) {
+    for(let i=0; i<this.data.length; i++){
+      if(this.data[i].gen == 'gen-0' && this.data[i].gen == gen){
+        this.myData.push(this.data[i]);
+      }
+      if(this.data[i].gen == 'gen-1' && this.data[i].gen == gen){
+        this.myData.push(this.data[i]);
+      }
+      if(this.data[i].gen == 'gen-2' && this.data[i].gen == gen){
+        this.myData.push(this.data[i]);
+      }
+    }
+
+    this.showData = this.myData;
+    this.myData = [];
+    this.linkActive = nbutton;
+  }
 }
