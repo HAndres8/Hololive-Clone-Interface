4fc9505bd3840092682735dff3ed7a788869860d
HAndres8 <andresfhernandez999@gmail.com>
2024-08-02 23:08:45 -0500

    Updating versions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ListTalentsComponent } from './components/list-talents/list-talents.component';
@@ -10,20 +10,13 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { TagsComponent } from './components/tags/tags.component';
 import { FooterComponent } from './components/footer/footer.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    ListTalentsComponent,
-    NavbarComponent,
-    TagsComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        ListTalentsComponent,
+        NavbarComponent,
+        TagsComponent,
+        FooterComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
